38d105ae21971717ca4455f78d5bce63eb363c35
ANDFS2411x <andres2020k@gmail.com>
2025-10-28 20:05:17 -0500

    Path of file changed.

--- det_track_count/configuracion.py
+++ det_track_count/configuracion.py
@@ -4,8 +4,7 @@ INPUT_W = 640
 INPUT_H = 640
 CONF_THRESH = 0.30
 IOU_THRESHOLD = 0.65
-ENGINE_PATH = "../best4_100epocas.engine"
-
+ENGINE_PATH = "/home/andfs/portal-inteligente-personas/modelos/YoloV5s/100_Epocas/best4_100epocas.engine"
 CAP_QUEUE_MAX = 5          # Cola captura → detección
 VIZ_MAX_FPS = 10           # FPS máximo para preview
 SHOW_FPS_ON_FRAME = True   # Mostrar FPS en el frame
