9520ca7936145e449008f95d172fa09e15557f7b
ANDFS2411x <andres2020k@gmail.com>
2025-11-05 16:14:32 -0500

    Fix warning of buildings.

--- cpp_det_track_count_yolov5-sort/build.sh
+++ cpp_det_track_count_yolov5-sort/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
-g++ -O3 -std=c++14   main.cpp yolov5_trt.cpp   -I. -I/usr/include/aarch64-linux-gnu -I/usr/local/cuda/include   `pkg-config --cflags --libs opencv4`   -L/usr/local/cuda/lib64   -lnvinfer -lcudart -lpthread -o portal
+g++ -O3 -std=c++17 main.cpp yolov5_trt.cpp \
+  -I. -I/usr/include/aarch64-linux-gnu -I/usr/local/cuda/include \
+  `pkg-config --cflags --libs opencv4` \
+  -L/usr/local/cuda/lib64 -lnvinfer -lcudart -lpthread -lstdc++fs \
+  -Wno-deprecated-declarations -o portal
+
 echo "✅ Compilado: ./portal"
